0b149eda868546401da88b5f63129ec45c350d27
dianaparr <dianaparra.9611@gmail.com>
2022-01-05 12:50:07 -0500

    add feature most important such as includes and pow

---
+++ src/es7/index.js
@@ -0,0 +1,20 @@
+// nace en junio de 2016
+
+// 1
+// método includes, para saber si hay un elemento dentro
+
+let numbers = [1,2,3,7,8];
+
+if (numbers.includes(7)) {
+    console.log('Sí se encuentra el valor 7');
+} else {
+    console.log('No se encuentra.')
+}
+
+// 2
+// elevar a la potencia de...
+
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+console.log(result);
