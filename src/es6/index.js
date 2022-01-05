9ae7648ba21791c264ef4a20ee5f5e91ed852b6a
dianaparr <dianaparra.9611@gmail.com>
2022-01-05 11:41:43 -0500

    add new features such as spread operator, let and const

--- src/es6/index.js
+++ src/es6/index.js
@@ -27,3 +27,70 @@ console.log(epicPhrase);
 // ES6 template literals
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+// 3
+
+// multilinea en los strings
+let lorem ="Quiero escribir una super frase para separar \n"
++ "y demostrar que hay otra forma de hacerlo."
+
+// ES6
+let lorem2 = `otra frase para demostrar que podemos
+separar las frases`
+
+console.log(lorem);
+console.log(lorem2);
+
+// 4
+
+// Desestructuración de elementos
+
+let person = {
+    'name': 'Diana',
+    'age': 25,
+    'country': 'CO'
+}
+
+console.log(person.name, person.country);
+
+// ES6
+let { name, age, country } = person;
+console.log(name, age, country);
+
+// 5
+
+// operador de propagación (expandir elementos)
+
+let team1 = ['Diana', 'Juan', 'Camila'];
+let team2 = ['Julian', 'Carlos', 'Laura'];
+
+//Unión de elementos
+let education = ['David', ...team1, ...team2];
+console.log(education);
+
+// 6
+
+// no se usa var, se usa let -> con let la variable esta disponible en el bloque de código en donde es llamado
+// NOTA: Es mas conveniente empezar a trabajar con let
+
+// globalVar sí es accesible
+{
+    var globalVar = "Global Var";
+}
+
+//globalLet no es accesible en OTRO scope, se puede hacer accesible DENTRO de este mismo bloque de código
+{
+    let globalLet = "Global Let";
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+// 7
+
+// const -> permite establecer una variable constante, nunca cambia su valor -> No permite la reasignación
+const a = 'b';
+a = 'a';
+
+// TypeError: Assignment to constant variable.
+console.log(a);
