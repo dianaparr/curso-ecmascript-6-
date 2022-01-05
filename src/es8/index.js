6a194f504fe90ea2615cadda01471abe4cb92e97
dianaparr <dianaparra.9611@gmail.com>
2022-01-05 13:02:40 -0500

    add features to es8

---
+++ src/es8/index.js
@@ -0,0 +1,44 @@
+// se lanza en junio de 2017
+
+// 1
+// object.entries -> retorna la key y value de un array de arrays
+const data = {
+    frontend: 'Juan',
+    backend: 'Diana',
+    design: 'Camilo'
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+
+// para saber cuántos elementos tiene un objeto
+console.log(entries.length);
+
+// 2
+// object values -> retorna los valores de un objeto en un array
+const data = {
+    frontend: 'Juan',
+    backend: 'Diana',
+    design: 'Camilo'
+};
+
+const values = Object.values(data);
+console.log(values);
+console.log(values.length);
+
+
+// 3
+// padding -> anteponer elementos a un string
+
+const string = 'hello';
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(12, ' -----'));
+
+console.log('food'.padEnd(12, ' ------'));
+
+
+// Trailing commas: pueden ir las comas al final de cada elemento dentro del objeto así se vaya o no a agregar mas elementos en el objeto
+
+const obj = {
+    item: 'casa',
+}
