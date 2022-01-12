a7f13c8b77deb15bcdd29e49f6dedde8b7720ec3
dianaparr <dianaparra.9611@gmail.com>
2022-01-12 10:35:06 -0500

    add new feature async and await

--- src/es8/index.js
+++ src/es8/index.js
@@ -42,3 +42,35 @@ console.log('food'.padEnd(12, ' ------'));
 const obj = {
     item: 'casa',
 }
+
+
+// 4
+// Async y await
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(() => resolve("Hello World"), 3000)
+        : reject(new Error("Test Error"))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsync();
+
+
+// manejo correcto de errores con try y catch
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+anotherFunction();
