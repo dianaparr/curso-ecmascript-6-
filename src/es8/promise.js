6aef64ae7ec3616b54594b4bed7c9af66c852fad
dianaparr <dianaparra.9611@gmail.com>
2022-01-12 15:55:32 -0500

    add example promise with callbacks

--- src/es8/promise.js
+++ src/es8/promise.js
@@ -1,4 +1,4 @@
-// Ejemplo de promesa
+// 1. Ejemplo de promesa
 // Si el número que generamos aleatoriamente es mayor a 5 entonces se resuelve, si es menor a 5 entonces es rechazada y obtenemos un error
 
 const promise = new Promise((resolve, reject) => {
@@ -17,7 +17,7 @@ promise
     .catch(error => console.error(error));
 
 
-// función que recibe parámetros y retorna la instancia de Promise
+// 2. función que recibe parámetros y retorna la instancia de Promise
 
 function randomDelayed(max=10, expected=5, delay=1000) {
     return new Promise((resolve, reject) => {
@@ -38,3 +38,24 @@ function randomDelayed(max=10, expected=5, delay=1000) {
 randomDelayed(100, 75, 2500)
     .then(number => console.log(number))
     .catch(error => console.error(error));
+
+
+// 3. pasando de Callback a promesas
+// leer un archivo usando el módulo `fs` de Node.js
+
+import fs from 'fs';
+
+function readFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) return reject(err);
+            return resolve(data);
+        });
+    });
+}
+
+readFile('./archivo.txt')
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+
+
